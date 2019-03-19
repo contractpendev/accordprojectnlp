08eb2a2415719efe7fbe005f7e35c7e99cad2226
Philip Andrew <philip@contractpen.com>
2019-03-19 17:34:52 +0800

    Initial commit

---
+++ src/api/MoviesApi.ts
@@ -0,0 +1,11 @@
+import IResource from "typings/IResource";
+
+export default class MoviesApi implements IResource {
+    
+    create(data: any) {
+        throw new Error("Method not implemented.");
+    }    findMany(): any[] {
+        throw new Error("Method not implemented.");
+    }
+
+}
